1ee8c7b95e52dba76512de23f731147fda1e3499
strand93 <strand.93@hotmail.com>
2021-02-14 11:42:26 +0100

    Update footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,12 +141,12 @@ export default () => (
       </div>
     </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+      <h3 className="text-5xl font-semibold">Start your adventure!</h3>
       <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        Ready to become the green expert your aspire to be, join the commuinty of Viridis and start that adventure!
       </p>
       <p className="mt-8">
-        <Button size="xl">Get Started Now</Button>
+        <Button size="xl">Register your interest</Button>
       </p>
     </section>
   </Layout>
