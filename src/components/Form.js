9f806b5de69ff780925312e9c7d0bd201a6e0abc
strand93 <strand.93@hotmail.com>
2021-02-15 13:49:55 +0100

    Remove comment

--- src/components/Form.js
+++ src/components/Form.js
@@ -23,10 +23,6 @@ export default function Form(){
     <div>
       <div id="interest-form" className="container mx-auto flex text-left">  
         <form className="mx-auto bg-white rounded-lg px-5 py-2 mt-5" onSubmit={sendEmail}>
-          {/*<div className="mx-auto">
-            <legend className="mx-auto font-medium text-gray-900">Features</legend>
-          </div>
-          */}
           <div>
             <input id="guides" name="guides" type="checkbox" className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
             <label htmlFor="guides" className="px-4 font-medium text-gray-700">Guides</label>
