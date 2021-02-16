17f1cdcd1c9e433203684d6f6c56ecc725e2d9e5
strand93 <strand.93@hotmail.com>
2021-02-16 21:01:34 +0100

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+import HeroImage from '../svg/HeroImage';
+
+export default ()  =>
+(
+    <div className="container p-10 mx-auto w-1/2 text-center">
+        <h1 className="text-4xl pl-12">Page not found</h1>
+        <HeroImage />
+    </div>
+);
