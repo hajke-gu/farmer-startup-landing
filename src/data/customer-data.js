0996399575a075068bd5d33e902f91cfdc20c092
strand93 <strand.93@hotmail.com>
2021-02-17 13:29:25 +0100

    Update statement

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -4,7 +4,7 @@ export default [
   {
     title: 'Perfect!',
     content:
-      "I'm looking forward to start using Viridis, to help me become an expert at plants!",
+      "\"I'm looking forward to start using Viridis, to help me become an expert at plants!\"",
     customerName: 'Zhichuan Li',
     customerTitle: 'Software Engineer',
     customerImage: farmer
@@ -12,7 +12,7 @@ export default [
   {
     title: 'Marvelous and thrilling!',
     content:
-      'I can finally sell my vegetables and plants in an easy way! Maybe I will meet some like minded people as well!',
+      '\"I can finally sell my vegetables and plants in an easy way! Maybe I will meet some like minded people as well!\"',
     customerName: 'Agda Nilsson',
     customerTitle: 'Grandma and garden expert',
     customerImage: farmer
@@ -20,7 +20,7 @@ export default [
   {
     title: 'Helpful',
     content:
-      'Finally I place where I dont have to stand in queue for 10 years to find a cultivation plot.',
+      '\"Finally I place where I dont have to stand in queue for 10 years to find a cultivation plot.\"',
     customerName: 'Pelle Pellesson',
     customerTitle: 'Impatient man',
     customerImage: farmer
